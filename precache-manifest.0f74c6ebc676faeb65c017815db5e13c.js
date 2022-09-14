e307604054ac48e642374005131305d1c1c3552f
ElTraffy <traffydeal@gmail.com>
2022-09-15 00:32:13 +0100

    Updates

---
+++ precache-manifest.0f74c6ebc676faeb65c017815db5e13c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58cd5f29152b99968cf6",
+    "url": "/static/css/main.5fbb8536.chunk.css"
+  },
+  {
+    "revision": "58cd5f29152b99968cf6",
+    "url": "/static/js/main.58cd5f29.chunk.js"
+  },
+  {
+    "revision": "08513ed017013cc0d083",
+    "url": "/static/js/1.08513ed0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "b660ee5777c8821aba4a2c364eb92fe2",
+    "url": "/index.html"
+  }
+];
