1284c6e94c07dbab52d43d56b5bb59137e707114
ElTraffy <traffydeal@gmail.com>
2022-09-14 19:00:54 +0100

    Header done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5> Hello I'm</h5>
+        <h1> Sebastian Freitas</h1>
+        <h5 className = "text-light">Game Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+
+    </header>
   )
 }
 
